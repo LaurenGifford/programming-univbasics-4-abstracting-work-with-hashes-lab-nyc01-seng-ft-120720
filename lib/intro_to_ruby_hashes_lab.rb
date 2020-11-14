d548ca97397d9cfebf03219ce64d610643e6e215
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-14 16:11:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,17 +1,15 @@
 def my_hash_creator(key, value)
-  {
-    key: 'value'
-  }
+  {key => value}
 end
 
 def read_from_hash(hash, key)
-  hash[:key]
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[:key]
-    hash[:key] += 1
+  if hash[key]
+    hash[key] += 1
   else 
-    hash[:key] = 1
+    hash[key] = 1
   end
 end
